af6be3da80d191c3e82737c681e9ce5acc6dd1b3
pubestpubest <pubestpubest@gmail.com>
2025-04-01 01:40:49 +0700

    change deprecated jwt function

--- main.go
+++ main.go
@@ -158,7 +158,7 @@ func loginUser(db *gorm.DB, c *fiber.Ctx) error {
 
 func authRequired(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	})
 	if err != nil || !token.Valid {
